5cea2cf15b0730c6008b06f657f14b263c534a66
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-01-07 09:52:09 -0700

    Started personal project

---
+++ Personal_Projects/financial_calc.py
@@ -0,0 +1,8 @@
+# LD 1st Financial Calculator
+
+# OVERVIEW
+# How long it will take to save for a goal based on a weekly or monthly deposit
+# Compound Interest Calculator
+# Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+# Sale Price Calculator (apply discounts to prices)
+# Tip Calculator
