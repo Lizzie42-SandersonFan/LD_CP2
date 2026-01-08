d08f171a4c3ca78ee3c8de2f9f3845dc846b62b9
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-01-08 09:52:31 -0700

    One function built

--- Personal_Projects/financial_calc.py
+++ Personal_Projects/financial_calc.py
@@ -6,3 +6,54 @@
 # Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
 # Sale Price Calculator (apply discounts to prices)
 # Tip Calculator
+
+
+# FUNCTIONS
+
+# Saving function: Know whether it is a weekly or monthly deposit. Get user's deposit and what end goal is. calculate
+def saving():
+    # Get frequency
+    while True:
+        frec = input("First:\nIs this a WEEKLY or MONTHLY deposit?\n").strip().upper()
+        if frec == "WEEKLY":
+            # valid input. Break the loop and keep going
+            break
+        elif frec == "MONTHLY":
+            # valid input. Break the loop and keep going
+            break
+        else:
+            print("Please make sure you entered your choice correctly.")
+            continue
+    # Get how much they are depositing every time
+    deposit = float(input("How much are you depositing every time? Omit '$' and commas\n"))
+    # Get end goal
+    goal = float(input("What is your end goal? Omit '$' and commas\n"))
+
+    def calculate():
+        nonlocal deposit
+        nonlocal goal
+        time = goal / deposit
+        return time
+    time = calculate()
+
+    if frec == "WEEKLY":
+        # Tell how many weeks
+        print(f"It will take you {time} weeks to save ${goal} with a weekly deposit of ${deposit}")
+    elif frec == "MONTHLY":
+        # Tell how many months
+        print(f"It will take you {time} months to save ${goal} with a monthly deposit of ${deposit}")
+    else:
+        # Something went terribly worng. how did this happen
+        print("What in God's name did you do to my code?")
+
+# Compound intrest: Get starting amount, intrest rate, and how long it is being left in for. Tell them what the amount would be at the end of that time
+
+# Budget allocator: Get user's catigories and percent for each one, as well as salary. Make sure percents are exactly 100. divide salary into different catigories then tell the user what the math results are
+
+# Sale price: Get BASE ORIGINAL price and the discount. tell user new price
+
+# Tip: Get the amount and how much of a tip they want to leave. Tell user tip amount and what the final total would then be
+
+# Greet: Greet the user and have a menu corresponding to the different calculations that could be done
+
+saving()
