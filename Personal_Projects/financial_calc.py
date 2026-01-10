4a291a40eef7b2d72aaee156144fade850e2bb34
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-01-10 00:57:38 +0000

    Sync

--- Personal_Projects/financial_calc.py
+++ Personal_Projects/financial_calc.py
@@ -47,13 +47,88 @@ def saving():
         # Something went terribly worng. how did this happen
         print("What in God's name did you do to my code?")
 
-# Compound intrest: Get starting amount, intrest rate, and how long it is being left in for. Tell them what the amount would be at the end of that time
+# Compound intrest: Get starting amount, intrest rate, and how long it is being left in for. Tell them what the amount would be at the end of that time. Equation : final_result = deposit(1+(intrest/12))^(12*years left in)
+def compound():
+    # Get how much money the user is depositing
+    get_money = float(input("How much money are you depositing? Omit '$' and commas.\n"))
+    money = round(get_money, 2)
+    # Get how many years the user is leaving the money
+    get_years = float(input("How many years are you leaving the money in for? Type a number and omit commas.\n"))
+    years = round(get_years, 2)
+    # Get the intrest rate
+    get_intrest = float(input("What is the intrest rate? Enter as a decimal.\n"))
+    intrest = round(get_intrest, 2)
+    int_intrest = intrest*100
+
+    # Setting up the parts to be used in equation
+    def equation():
+        fraction = intrest / 12
+        roun_frac = round(fraction, 2)
+        exponant = 12 * years
+
+        ending_money = money*((1+roun_frac)**exponant)
+        rounded_money = round(ending_money, 2)
+
+        return rounded_money
+    end = equation()
+
+    # Tell how many weeks
+    print(f"If you leave ${money} in for {years} years with an intrest rate of {int_intrest}%, you will have ${end:,}.")
+    # Call greet func
 
 # Budget allocator: Get user's catigories and percent for each one, as well as salary. Make sure percents are exactly 100. divide salary into different catigories then tell the user what the math results are
+def budget(): # NOT DONE YET
+    # Lists for budget catigory and percentage
+    catigories = []
+    percents = []
+    budgets = []
+    # Get user's salary
+    salary = float(input("What is your monthly salary?\n"))
+    # Ask user how many different catigories they want to budget for
+    num_of_catigories = int(input("How many catigories do you want to budget for? (Enter whole number)\n"))
+    while True:
+        # Do a loop based on number of catigories
+        for x in range(num_of_catigories):
+            catigory = input(f"What is the name of the {x+1} catigory?\n")
+            catigories.append(catigory)
+        for y in catigories:
+            percent = float(input(f"What is the percentage for {y}? Enter as a decimal\n"))
+            percents.append(percent)
+        # When loop is done, see if all percents are correct (add up to 100)
+        total = 0
+        for num in percents:
+            total += num
+        if total == 1:
+            # all percents are valid
+            break
+        else:
+            # There must be something wrong with the numbers
+            print("It seems like that the percentages you entered don't add to 100%.\nPlease try again.")
+            continue
+        # if all percents good, leave while loop and start doing the math
+    # Get Ms. LaRose's help on being able to work on two lists at once
+    # Tell user final results
 
 # Sale price: Get BASE ORIGINAL price and the discount. tell user new price
+def onSale():
+    og_price = float(input("What is the original price of the item? Omit '$' and commas\n"))
+    int_sale = float(input("What is the discount in the item? Omit '%'\n"))
+    percent = int_sale / 100
+
+    def subtractDiscount():
+        nonlocal og_price
+        nonlocal percent
+
+        take_away = og_price * percent
+        new_price = og_price - take_away
+        return new_price
+    discounted = subtractDiscount()
+    print(f"The item now costs ${discounted:.2f}")
+    # Call greet func
 
 # Tip: Get the amount and how much of a tip they want to leave. Tell user tip amount and what the final total would then be
+def leaveTip():
+    pass
 
 # Greet: Greet the user and have a menu corresponding to the different calculations that could be done
 def greet():
