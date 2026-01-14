8e3ca3c9d5565b31d52cc29e8484831dd5353e86
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-01-14 09:52:09 -0700

    Lots of comments

---
+++ Personal_Projects/personal_library.py
@@ -0,0 +1,15 @@
+# LD 1st Personal Library Program
+
+# OVERVIEW
+# Create a program that allows user to manage a personal library catalog for any ONE type (books, movies, music, etc). The project needs to allow users to add new items, display ALL contents, search for a specific item (by title, author/artist/director), remove a book from the library, exit the program. 
+
+# STEPS
+# Stores all items in a list
+# Function to add a new item
+# Function to search the items
+# Function to remove an item
+# Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
+# Project must include
+    # easy to understand variable and function names
+    # Pseudocode comments explaining what the code is doing
+    # Good use of white space to keep items separate and easy to read/find
