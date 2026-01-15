8b600fd49e1852251affcc45c8aecbbd32990d14
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-01-15 09:52:40 -0700

    Comments

--- Personal_Projects/personal_library.py
+++ Personal_Projects/personal_library.py
@@ -13,3 +13,16 @@
     # easy to understand variable and function names
     # Pseudocode comments explaining what the code is doing
     # Good use of white space to keep items separate and easy to read/find
+
+
+# FUNCTIONS
+
+# Run game: First build all iner functions. Then establish the list of books. Have a while loop running the program with the option to add, view, remove, search, and leave
+
+    # Add items: First, ask the user for book title. Then ask user for author. Combine thoes two into one item. Append that combined item. Done
+
+    # Remove Items: 
+
+    # View?
+
+    # Search for something:
