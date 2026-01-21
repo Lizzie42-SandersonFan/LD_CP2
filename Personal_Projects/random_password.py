b9ccad96c1370d2e236ff054b2f92277226325ba
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-01-21 09:52:53 -0700

    Psuedocode

---
+++ Personal_Projects/random_password.py
@@ -0,0 +1,28 @@
+# LD 1st Random Passowrd Generator
+
+# OVERVIEW
+# Create a program that allows a user to specify password requirements (length, upper/lowercase letters, numbers, and special characters) then gives them 4 possible passwords they could use. 
+
+# STEPS
+# A main function that runs the code
+# Functions for the different password requirements
+# A function that assembles that password once it is the correct length
+# Users should be able to specify length and if they want to include
+    # uppercase letters
+    # lowercase letters
+    # numbers
+    # special characters
+# HINT: You can make this by randomly selecting from different lists OR by randomly generating the ASCII letters! 
+
+# Main Func: Build inner functions, greet user, ask user for password paramiters. Use inner functions to build four different passwords. Show user the four different passwords
+    # func for length. Ask user how long it needs to be. return length
+    
+    # func for uppercase needed. Yes/No. return yes/no
+
+    # func for lowercase needed. Yes/No. return yes/no
+
+    # func for numbers needed. Yes/No. return yes/no
+
+    # func for special characters needed. Yes/No. return yes/no
+
+    # func for assembly. 
