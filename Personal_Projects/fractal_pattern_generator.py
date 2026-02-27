add3ca30e82dc9b9dd598d493171c7aa237d92dc
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-02-27 09:52:03 -0700

    Save

--- Personal_Projects/fractal_pattern_generator.py
+++ Personal_Projects/fractal_pattern_generator.py
@@ -22,17 +22,21 @@ def main():
         hexa_code = re.compile(r'^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$')
         return bool(re.match(hexa_code, string))
     
+    def draw_fancy_triforce():
+        pass
+    
     # Greet user
     while True:
         depth = input("Enter Recursion Depth for your Triangle (1-5):\n")
         if depth == "1" or depth == "2" or depth == "3" or depth == "4" or depth == "5":
             # Valid input
+            depth = int(depth)
             break
         else:
             print("Invalid input. Try again")
             continue
     while True:
-        color = input("Enter the hex code for the color you would like (Ex: #FFFFFF, #000000, #1656AD):\n")
+        color = input("Enter the hex code for the LINE color you would like (Ex: #FFFFFF, #000000, #1656AD):\n")
         if is_valid_hexa_code(color):
             # Valid color
             print("Yes")
@@ -40,5 +44,26 @@ def main():
         else:
             print("Invalid input. Please try again")
             continue
+    while True:
+        back_color = input("Enter the hex code for the BACKGROUND color you would like (Ex: #FFFFFF, #000000, #1656AD):\n")
+        if is_valid_hexa_code(back_color) and back_color != color:
+            # Valid color
+            print("Yes")
+            break
+        else:
+            if is_valid_hexa_code(back_color) == False:
+                print("Invalid color. Try again")
+                continue
+            elif back_color == color:
+                print("Your background color cannot be the same as your lines. Pick a different color")
+                continue
+            else:
+                print("What in world happened!?!?")
 
 main()
+
+# LOGIC FOR DRAWING FANCY TRIFORCE
+# See fancy_triforce.png
+# Am I drawing big triangle first or small triangles first?
+    # small triangles first
+# No ideas yet on what I would do if drawing small ones first
