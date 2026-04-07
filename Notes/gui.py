c3b4616921a9e83175575f143b80b2901d6564bb
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2026-04-07 09:05:44 -0600

    Fix Notes

--- Notes/gui.py
+++ Notes/gui.py
@@ -16,11 +16,12 @@ label.config(fg = "purple", background="orange")
 root.count = 0
 def add():
     root.count += 1
-    tk.Label(root, text=root.count).pack()
+    num["text"] = root.count
 
 btn = tk.Button(root, text="ADD", command=add)
 btn.pack()
-
+num = tk.Label(root, text="0")
+num.pack()
 
 label.pack()
 #image = tk.PhotoImage(file = "Notes\imgs\swiss_cheese.png")
